329781292a1417715ad4b0789e8fba3e5f4c8072
ZeroBoom <rsh921015@gmail.com>
2020-10-18 16:24:04 +0900

    1. 캐릭터, 캐릭터 움직임 구현 2. 총알, 총알 생성 및 파괴 구현

---
+++ top_shooting/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        transform.position = transform.position + new Vector3(0, 0.1f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ top_shooting/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+
+    private int bulletTerm = 0;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + new Vector3(-0.1f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(0.1f, 0);
+        }
+
+        if (bulletTerm++ >= 20)
+        {
+            var bulletObject = Instantiate(BulletPrefab);
+            bulletObject.transform.position = transform.position;
+
+            bulletTerm = 0;
+        }
+    }
+}
