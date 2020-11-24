9a3a1708b9ffaefb0ec73ef7e91915bfc17c64fc
ZeroBoom <rsh921015@gmail.com>
2020-11-24 20:56:15 +0900

    1. server 구조 작업 2. 랭킹 구현

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : process.env.PASSWORD,
+  database : 'top_shooting'
+});
+connection.connect();
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+    });
+});
+
+app.get('/ranking', (req, res) => {
+    const rankings = [];
+
+    connection.query(`SELECT * FROM userscores;`, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+        }
+
+        console.log(results);
+
+        results.sort((a, b) => b.score - a.score);
+
+        res.json(results);
+    });
+});
+
+app.post('/registerScore', (req, res, next) => {
+    var name = req.body.name;
+    var score = req.body.score;
+
+    connection.query(`INSERT INTO userscores VALUES ('${name}', ${score}) ON DUPLICATE KEY UPDATE score = ${score};`, (error, results, fields) => {
+        if (error) {
+            console.log(error);
+        }
+
+        console.log(results);
+    });
+
+    res.json({success: true});
+});
+
+app.listen(port, () => {
+    console.log(`server is listening at localhost:${port}`);
+});
